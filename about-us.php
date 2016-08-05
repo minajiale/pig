f060a8509e2b54f1e8a1eae20783066930cba8e2
minajiale <13177918633@163.com>
2016-08-05 15:29:11 +0800

    perfect it

---
+++ about-us.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>关于我们</title>
+</head>
+<link rel="stylesheet" href="./css/commen.css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="./css/about.css" media="screen" title="no title" charset="utf-8">
+<body>
+  <?php include "head.php" ?>
+<div class="we">
+  <h1>关于我们</h1>
+  <p>
+　　“小猪理财”（www.xiaozhu158.com）是一家安全、便捷、透明的互联网金融信息服务中介平台，由江西小猪金融信息
+服务有限公司推出经营。小猪理财结合市场形势，开创出“机构+现金双重担保”特色的商业模式，是国内
+首创专业垂直深耕与市政工程，政府基础建设的工程投标保证金的优质资产项目，真正做到项目的足够安全与优质。同时
+，通过资金闭环式的监管体系，通过设立银行共管账户，监管资金去向，做到资金闭环，交易闭环
+全方位保障投资者资金安全，至今保持着“零逾期，零坏账”的行业记录。迄今为止，平台已覆盖至全国，加盟商分布全国30多个省份，成立
+数百家网络运营中心。
+  </p>
+</div>
+<ul class="icon">
+  <li>
+  <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_09.jpg" width="50" height="40" alt="">
+  <p>11亿2325万元</p>
+  <p>平台交易总额</p>
+  </li>
+  <li>
+    <img src="images/money.jpg" width="50" height="40" alt="">
+    <p>24501笔</p>
+    <p>交易笔数</p>
+  </li>
+  <li>
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_09.jpg" width="50" height="40" alt="">
+    <p>2088.46万元</p>
+    <p>为用户赚取收益</p>
+  </li>
+  <li>
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_03.jpg" width="50" height="40" alt="">
+    <p>505天</p>
+    <p>安全运营天数</p>
+  </li>
+</ul>
+<div class="group">
+  <h1>团队简介</h1>
+  <table>
+    <tr>
+      <td></td>
+      <td>	<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_15.jpg" width="207" height="446" alt=""></td>
+      <td>	<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_16.jpg" width="207" height="446" alt=""></td>
+      <td>	<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_18.jpg" width="207" height="446" alt=""></td>
+      <td>	<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_20.jpg" width="207" height="446" alt=""></td>
+      <td>	<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_22.jpg" width="207" height="446" alt=""></td>
+      <td></td>
+    </tr>
+  </table>
+</div>
+<div class="companey">
+  <h1>企业资质</h1>
+  <table>
+    <tr>
+      <td><img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_30.jpg" width="253" height="188" alt=""></td>
+      <td><img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_32.jpg" width="253" height="188" alt=""></td>
+      <td><img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_34.jpg" width="253" height="188" alt=""></td>
+      <td><img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_37.jpg" width="253" height="188" alt=""></td>
+    </tr>
+  </table>
+</div>
+<div class="people">
+  <h1>员工风采</h1>
+  			<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_43.jpg" width="1096" height="498" alt="">
+</div>
+<div class="develope">
+  <h1>发展历程</h1>
+  <div class="process">
+<a href="#">&lt</a>
+<img src="./images/canvas.png " alt="" />
+<a href="#">&gt</a>
+  </div>
+</div>
+<div class="touch">
+  <h1>联系我们</h1>
+  <div class="table">
+    <div class="">
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_47.jpg" width="94" height="93" alt="">
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_49.jpg" width="94" height="93" alt="">
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_51.jpg" width="94" height="93" alt="">
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_53.jpg" width="94" height="93" alt="">
+    <img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_55.jpg" width="94" height="93" alt="">
+        </div>
+    <div class="">
+      <p>公司地址</p>
+      <p>客服电话</p>
+      <p>公司邮箱</p>
+      <p>Q投资交流群</p>
+      <p>微信公众号</p>
+          </div>
+          <div class="">
+        <p> 江西省南昌市红谷滩新区 红谷中大道浦发银行大厦12楼  </p>
+      <p>400-836-7676</p>
+      <p>service@xiaozhu158.com</p>
+      <p>431818563</p>
+      <p>xiaozhu_58</p>
+    </div>
+</div>
+<div class="road">
+  	<img src="images/&#x5173;&#x4e8e;&#x6211;&#x4eec;_63.jpg" width="603" height="266" alt="">
+    <p>小猪理财考察线路</p>
+    <ul>
+      <li>公司地址：江西省南昌市红谷滩新区红谷中大道浦发银行大厦12楼</li>
+      <li>火车西站线路：<span>   高铁1号线、2号线，秋水广场下车，向西300米</span></li>
+      <li>火车西站线路：<span>221路公交车至“鼎峰中央”站下车</span></li>
+      <li>昌北机场线路：<span>机场一号专线秋水广场下车，向西300米</span></li>
+    </ul>
+</div>
+<?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ funancial.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>关于我们-财务凭证</title>
+</head>
+<link rel="stylesheet" href="./css/commen.css">
+<link rel="stylesheet" href="./css/join-us.css">
+<body>
+<?php include 'head.php'; ?>
+<div class="join">
+  <ul>
+    <li><img src="./images/dot.gif" alt="" />  <a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+    <li><img src="./images/dot.gif" alt="" /><a href="">2016年7月11日 北京市西城区运营中心正式成立</a><span>2019-07-11</span></li>
+  </ul>
+    <div class="page">
+      <a href="#">首页</a>
+      <a href="#">上一页</a>
+      <a href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">7</a>
+      <a href="#">8</a>
+      <a href="#">9</a>
+      <a href="#">........</a>
+      <a href="#">下一页</a>
+      <a href="#">尾页</a>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
+</body>
+</html>
